16dca54e50f9b1a73e48d2933a91088b4b28aaea
SariaBarros <sandy.vitoriabarros@gmail.com>
2022-10-31 17:19:26 -0400

    Lista de exercicio 1

---
+++ br/com/petshop/businessobject/Internacoes.java
@@ -0,0 +1,29 @@
+package br.com.petshop.businessobject;
+
+import br.com.petshop.modelo.Cachorro;
+
+import java.util.Objects;
+
+public class Internacoes {
+
+    private Cachorro[] cachorrosInternados = new Cachorro[10];
+
+    public void internar(Cachorro cachorro){
+        for (int i = 0; i < cachorrosInternados.length; i++) {
+            if(Objects.isNull(cachorrosInternados[i])){
+                cachorrosInternados[i] = cachorro;
+                break;
+            }
+        }
+    }
+
+    public void mostraCachorrosInternados(){
+        System.out.println("-----------------DOGUINHOS DOENTES--------------------");
+        for (Cachorro cachorro: cachorrosInternados) {
+            if (Objects.nonNull(cachorro)) {
+                System.out.println(cachorro);
+            }
+        }
+    }
+
+}
---
+++ br/com/petshop/modelo/Cachorro.java
@@ -0,0 +1,95 @@
+package br.com.petshop.modelo;
+
+public class Cachorro {
+
+    // atributos, o que um classe tem
+    private String nome;
+    private String raca;
+
+    private int idade;
+    private boolean estaDoente;
+
+    /* atributo estatico, que pertence somente a classe
+    havera somente uma instancia, compartilhada entre os objetos
+     */
+    private static int contador;
+
+    // construtores, que podem ser usados para a criação de objetos
+    public Cachorro(){
+        contador++;
+    }
+
+    public Cachorro(String nome){
+        contador++;
+        this.nome = nome;
+    }
+
+    public Cachorro(String nome, int idade){
+        contador++;
+        this.nome = nome;
+        setIdade(idade);
+    }
+
+
+    // metodos, o que uma classe faz...
+
+    // metodos de configuração - setam os valores - setters dos atributos
+
+    public void setEstaDoente(boolean estaDoente) {
+        this.estaDoente = estaDoente;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    // encapsulando regra de negócio no setter
+    public void setIdade(int idade) {
+        if(idade < 0){
+            System.err.println("Cachorro não pode ter idade negativa");
+        } else if(idade > 29) {
+            System.err.println("Não é permitido cachorros com idades superiores a 29 anos");
+        } else {
+            this.idade = idade;
+        }
+    }
+
+    // metodos de consulta - consultam os valores dos atributos
+
+    public boolean isEstaDoente() {
+        return estaDoente;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    // metodo usado para imprimir mais informações (estado) sobre os valores dos atributos do objeto
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + nome + '\'' +
+                ", raca='" + raca + '\'' +
+                ", idade=" + idade +
+                ", estaDoente=" + estaDoente +
+                ", contador=" + contador +
+                '}';
+    }
+}
